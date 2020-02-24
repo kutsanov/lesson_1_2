6cf7dda0dd75b10f7147d41c360495e8186ec576
vovakutsanov <kutsanov@mail.ru>
2020-02-25 04:53:31 +0700

    lesson 1-2

--- lesson_1_2/ViewController.swift
+++ lesson_1_2/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var SignalRed: UIView!
+    @IBOutlet weak var SignakYel: UIView!
+    @IBOutlet weak var SignalGreen: UIView!
+    
+    
+    @IBOutlet weak var BtnGo: UIButton!
+    
+    var SignaNow: String = "green"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        SignalRed.layer.cornerRadius = 65
+        SignakYel.layer.cornerRadius = 65
+        SignalGreen.layer.cornerRadius = 65
+        SignalRed.alpha = 0.3
+        SignakYel.alpha = 0.3
+        SignalGreen.alpha = 0.3
     }
 
 
+    @IBAction func BtnGoAction() {
+        BtnGo.setTitle("Next", for: .normal)
+        if SignaNow == "green" {
+            SignalRed.alpha = 1
+            SignakYel.alpha = 0.3
+            SignalGreen.alpha = 0.3
+            SignaNow = "red"
+        } else if SignaNow == "red" {
+            SignalRed.alpha = 0.3
+            SignakYel.alpha = 1
+            SignalGreen.alpha = 0.3
+            SignaNow = "yel"
+        } else if SignaNow == "yel" {
+                   SignalRed.alpha = 0.3
+            SignakYel.alpha = 0.3
+                   SignalGreen.alpha = 1
+                   SignaNow = "green"
+               }
+        
+    }
+    
 }
 
